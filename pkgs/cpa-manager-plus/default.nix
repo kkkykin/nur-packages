c385734d41eda15e17a816fb2398db815a49b6fc
hermes <hermes@localhost>
2026-08-01 19:11:47 +0800

    cpa-manager-plus: 1.11.7 -> 1.11.11

--- pkgs/cpa-manager-plus/default.nix
+++ pkgs/cpa-manager-plus/default.nix
@@ -6,11 +6,11 @@
 
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "cpa-manager-plus";
-  version = "1.11.7";
+  version = "1.11.11";
 
   src = fetchurl {
     url = "https://github.com/seakee/CPA-Manager-Plus/releases/download/v${finalAttrs.version}/cpa-manager-plus_v${finalAttrs.version}_linux_amd64.tar.gz";
-    hash = "sha256-HDGY5NWVs0308Dv1B8cxdmd0fte2vSCJOl//WsR4zMI=";
+    hash = "sha256-uK7a1SjFcozvlpqZEKSru8fXxmdWXAtlzrj6DKwwwQM=";
   };
 
   dontBuild = true;
