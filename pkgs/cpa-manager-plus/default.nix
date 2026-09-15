3194737adcd3cb685f04c72720c01ab4928d3c8e
kkkykin <43497562+kkkykin@users.noreply.github.com>
2026-09-15 22:54:32 +0000

    auto: update packages

--- pkgs/cpa-manager-plus/default.nix
+++ pkgs/cpa-manager-plus/default.nix
@@ -7,11 +7,11 @@
 
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "cpa-manager-plus";
-  version = "1.12.12";
+  version = "1.12.13";
 
   src = fetchurl {
     url = "https://github.com/seakee/CPA-Manager-Plus/releases/download/v${finalAttrs.version}/cpa-manager-plus_v${finalAttrs.version}_linux_amd64.tar.gz";
-    hash = "sha256-pc6m+FnQ8801tfW8EFw5JOku6oENjr1U8A1JrswfZIw=";
+    hash = "sha256-d+ZzZbhs8jQ2jt/aTzXbJogBN4zDK60qvGr8CWFcAc0=";
   };
 
   dontBuild = true;
